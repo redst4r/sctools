136878ec4fa8adad0cb7709c9f15b918532e460b
redst4r <redst4r@web.de>
2021-09-22 19:00:22 -0700

    LR test for kbet

--- sctools/kbet.py
+++ sctools/kbet.py
@@ -8,7 +8,8 @@ import numpy as np
 def calc_kBET(
         data: AnnData,
         attr: str,
-        nn_key: 'distances'
+        nn_key='distances',
+        method='chi2'  # lr
 ):
     """
     TODO: the chi2 approximation is only valid for large number of neighbours.
@@ -58,9 +59,36 @@ def calc_kBET(
         K = A.sum(1).A.flatten()
         expected_counts = (K.reshape(-1, 1) * ideal_dist)
 
-    # Caculate the chi2 statistics
+    # Caculate the test
+    if method == "chi2":
+        stat, p_value = chi2test(expected_counts, observed_counts)
+    elif method == 'lr':
+        stat, p_value = lrtest(expected_counts, observed_counts)
+
+    return stat, p_value, expected_counts, observed_counts
+
+def chi2test(expected_counts, observed_counts):
     stat = np.sum(np.square(np.subtract(observed_counts.A, expected_counts)) / expected_counts, 1)
     dof = expected_counts.shape[1] -1
     p_value = 1 - chi2.cdf(stat, dof)
-
     return stat, p_value
+
+def lrtest(expected_counts, observed_counts):
+
+    observed_counts = observed_counts.A
+    d = np.sum(observed_counts == 0, axis=1)  # number of unobserved cats in each neighborhood
+    k = np.sum(observed_counts, axis=1)  # neighborhood size
+    g1 = 1 / (k + d)
+    g2 = observed_counts / (k + d).reshape(-1, 1)
+
+    adj_freqs = np.maximum(g1.reshape(-1, 1), g2)
+    exp_freqs = expected_counts / expected_counts.sum(axis=1, keepdims=True)
+    _t = observed_counts * np.log(exp_freqs/adj_freqs)
+    G2 = -2 * np.sum(_t, axis=1)
+
+    dof = expected_counts.shape[1] -1
+    p_value = 1 - chi2.cdf(G2, dof)
+    return G2, p_value
+
+
+# def mctest(expected_counts, observed_counts):

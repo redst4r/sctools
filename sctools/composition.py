c0ad1d5ebda72ba77d0898484dadb3caf27b5596
redst4r <redst4r@web.de>
2022-12-17 18:48:35 -0800

    added compositional data tools

---
+++ sctools/composition.py
@@ -0,0 +1,167 @@
+import numpy as np
+import pandas as pd
+import scanpy as sc
+from scipy.stats import gmean
+from scipy.spatial.distance import pdist, squareform
+from scipy.stats import dirichlet
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from crukiopy.colormaps import color_dict_diagnosis
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.cluster.hierarchy import linkage, leaves_list
+from sccoda.util import data_visualization as viz
+
+
+
+def log_geometric_mean(x, axis):
+    """
+    logarithm of the geometric mean along the given axis
+    """
+    D = x.shape[axis]
+    loggmean = 1/D * np.sum(np.log(x), axis=axis, keepdims=True)
+    return loggmean
+
+def clr_transform(x, axis):
+    """
+    center log ratio transform for x, the features to be considered (cell types...) are along axis
+    """
+    return np.log(x) - log_geometric_mean(x, axis)
+
+def aichinson_distance(x, axis=1):
+    """
+    Aitinson distance matrix of the compositional data x (in raw counts)
+    - does CLR transform and computes the distances in CLR-space
+    """
+    y = clr_transform(x, axis)
+
+    d = pdist(y)
+    return squareform(d)
+
+# clr is scale invariant
+# x = np.array([[1,2,0,4], [10,11,12,13]])
+# x_norm = x/x.sum(1, keepdims=1)
+# np.testing.assert_allclose(clr_transform(x, axis=1),  clr_transform(x_norm, axis=1))
+#
+def clr_transform_bayesian(x, axis):
+    """
+    Center log ratio transform, taking into account the multinomial uncertainty in the counts
+    It'll return samples from the CLR posterior in the 3rd dimension
+    """
+    # NOTE: this is WRONG, dont normalize before, we need the full counts for the dirichlet!!
+    # WRONG:     x_norm = x/x.sum(1, keepdims=1)
+
+    x_bayes = []
+    for i in range(len(x)):
+        x_bayes.append(dirichlet(0.5 + x[i]).rvs(1000).T)
+#         x_bayes.append(x[i])
+
+    ## samples x features x mcmc_samples
+    x_bayes = np.stack(x_bayes)
+
+    x_clr = clr_transform(x_bayes,axis=axis)
+    return x_clr
+
+def aichinson_distance_bayesian(x, axis=1):
+    """
+    Aitchinson distance with Bayesian uncertainty from the counting
+    3rd dimension will be samples from the posterior of distance d[i,j]
+    """
+    # estimate the clr transform (samples from the posterior)
+    x_clr_bayes = clr_transform_bayesian(x, axis)
+
+    # obs x features x mc_samples
+    # we need to calc an obs x obs distance matrix
+    # to get d(x_i,x_j) calcualte the distance between the respective mc_samples
+
+    n_obs, n_features, n_mc = x_clr_bayes.shape
+    d = np.zeros((n_obs, n_obs, n_mc))
+    for i in range(n_obs):
+        for j in range(n_obs):
+            xi = x_clr_bayes[i,:,:].T
+            xj = x_clr_bayes[j,:,:].T
+            _d_tmp = np.sqrt(np.sum((xi-xj)**2, axis=1))
+            assert _d_tmp.shape == (n_mc,)
+            d[i,j,:] = _d_tmp
+    return d
+
+
+def compositional_pca(adata_coda):
+    """
+    PCA on the compositional adata, based on the posterior mean of the data in CLR space
+    """
+    x_bayes_clr = clr_transform_bayesian(adata_coda.X, axis=1)
+#     D_bayes = aichinson_distance_bayesian(adata_coda.X)
+    clr_posterior = x_bayes_clr.mean(axis=2)
+
+    scaler = StandardScaler()
+    clr_posterior = scaler.fit_transform(clr_posterior)
+    pca = PCA()
+    x_pca_posterior = pca.fit_transform(clr_posterior)
+
+    principal_components_df = pd.DataFrame(pca.components_, columns=adata_coda.var.index )
+    return pca, scaler, x_pca_posterior, principal_components_df
+
+
+def plot_pca_with_uncertainty(pca, scaler, x_clr_bayes, components, color_vector=None):
+    # project the uncertainty
+    pca_dim1, pca_dim2 = components
+
+    for i in range(x_clr_bayes.shape[0]): # iteratign over samples
+        _x = x_clr_bayes[i].T
+        _x = scaler.transform(_x)
+        _x_pca = pca.transform(_x)
+        plt.scatter(_x_pca[:,pca_dim1], _x_pca[:,pca_dim2], s=1, alpha=0.5)
+
+    # if color_vector is None:
+    #     plt.scatter(x_pca_posterior[:,pca_dim1], x_pca_posterior[:,pca_dim2])
+    # else:
+    #     plt.scatter(x_pca_posterior[:,pca_dim1], x_pca_posterior[:,pca_dim2], c=color_vector)
+#         [color_dict_diagnosis[_] for _ in data_scanpy_1.obs.diagnosis]
+
+def plot_pca_loadings(principal_components_df, components=(0,1)):
+
+    # each row vector pca.components[i,:] is a single PC
+    c1, c2 = components
+    pc1 = principal_components_df.iloc[c1]
+    pc2 = principal_components_df.iloc[c2]
+
+    plt.scatter(pc1, pc2)
+    for i in range(len(pc1)):
+        plt.arrow(0,0,pc1.values[i], pc2.values[i], color='k')
+        plt.text(x=pc1.values[i], y=pc2.values[i], s=pc1.index[i])
+
+
+# actual hiereachical clustering on the posterior-mean distances
+def clustered_heatmap_from_sccoda_CLR(sccoda_adata, figsize=(15, 5), barcolormap=None):
+#     X = sccoda_adata[:, cluster_order].X.copy()
+#     X = X/X.sum(1, keepdims=True)  # divide by total number of cells per sample
+
+    diag_colors = [color_dict_diagnosis[_] for _ in sccoda_adata.obs.diagnosis]
+    procedure_colors = ['red' if _ =="biopsy" else "blue" for _ in sccoda_adata.obs.procedure]
+
+    X = sccoda_adata.X.copy()
+    D_bayes = aichinson_distance_bayesian(sccoda_adata.X)
+    D_posterior_mean = D_bayes.mean(axis=2)
+
+    df_cluster = pd.DataFrame(X/X.sum(axis=1, keepdims=True), columns=sccoda_adata.var.index, index=sccoda_adata.obs.index)
+
+    Z = linkage(squareform(D_posterior_mean), method='ward', optimal_ordering=True)
+    sns.clustermap(df_cluster.T, row_cluster=False, col_linkage=Z,
+                   col_colors=[diag_colors, procedure_colors],
+                   xticklabels=True, yticklabels=True, figsize=figsize,
+                   method='ward', #dendrogram_ratio=0.2,
+#                    cmap=sns.dark_palette("#69d", reverse=False, as_cmap=True),
+                   cmap=sns.color_palette("Blues"),
+)
+
+    ix = leaves_list(Z)
+    order = [sccoda_adata.obs.index[i] for i in ix]
+    fig = viz.stacked_barplot(sccoda_adata, feature_name="samples", figsize=(10,5),
+                              level_order=order, #cmap = godsnot_cmap
+                              cmap=barcolormap
+                              )
+    plt.xticks(rotation=90);
+    return df_cluster, Z
+
+

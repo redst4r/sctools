1af72899662a08b4a6a25a3548f1ad09050489ba
redst4r <redst4r@web.de>
2023-04-15 18:41:51 -0700

    added kraken results parsing

---
+++ sctools/kraken.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+
+"""
+some code to parse output of Kraken/Bracken metagenomics analysis
+"""
+
+def parse_bracken(fname):
+    df = pd.read_csv(fname, sep='\t')
+
+    assert set(['name', 'taxonomy_id', 'taxonomy_lvl', 'kraken_assigned_reads', 'added_reads', 'new_est_reads', 'fraction_total_reads']) == set(df.columns)
+
+    df['fraction_total_reads'] = df.new_est_reads / df.new_est_reads.sum()
+    df['name'] = df['name'].apply(lambda x: x.strip())
+    return df
+
+def parse_kraken_report(fname):
+    df = pd.read_csv(fname, sep='\t', header=None)
+    df.columns = ['percent', 'reads', 'unique_reads','hierarchy', 'tax_id', 'name']
+
+    df['name'] = df['name'].apply(lambda x: x.strip())
+    return df

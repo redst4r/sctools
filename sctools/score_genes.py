c410318498bd7e447dd245eac2aec919b6143c71
redst4r <redst4r@web.de>
2023-06-22 14:29:51 -0700

    moved cell-cycle scoring into score_genes

--- sctools/score_genes.py
+++ sctools/score_genes.py
@@ -218,6 +218,26 @@ def score_genes(
     return adata if copy else None
 
 
+def get_cellcycle_genes():
+    cell_cycle_genes = [
+        'MCM5', 'PCNA', 'TYMS', 'FEN1', 'MCM2', 'MCM4', 'RRM1', 'UNG', 'GINS2',
+        'MCM6', 'CDCA7', 'DTL', 'PRIM1', 'UHRF1', 'MLF1IP', 'HELLS', 'RFC2',
+        'RPA2', 'NASP', 'RAD51AP1', 'GMNN', 'WDR76', 'SLBP', 'CCNE2', 'UBR7',
+        'POLD3', 'MSH2', 'ATAD2', 'RAD51', 'RRM2', 'CDC45', 'CDC6', 'EXO1',
+        'TIPIN', 'DSCC1', 'BLM', 'CASP8AP2', 'USP1', 'CLSPN', 'POLA1', 'CHAF1B',
+        'BRIP1', 'E2F8', 'HMGB2', 'CDK1', 'NUSAP1', 'UBE2C', 'BIRC5', 'TPX2',
+        'TOP2A', 'NDC80', 'CKS2', 'NUF2', 'CKS1B', 'MKI67', 'TMPO', 'CENPF',
+        'TACC3', 'FAM64A', 'SMC4', 'CCNB2', 'CKAP2L', 'CKAP2', 'AURKB', 'BUB1',
+        'KIF11', 'ANP32E', 'TUBB4B', 'GTSE1', 'KIF20B', 'HJURP', 'CDCA3', 'HN1',
+        'CDC20', 'TTK', 'CDC25C', 'KIF2C', 'RANGAP1', 'NCAPD2', 'DLGAP5', 'CDCA2',
+        'CDCA8', 'ECT2', 'KIF23', 'HMMR', 'AURKA', 'PSRC1', 'ANLN', 'LBR',
+        'CKAP5', 'CENPE', 'CTCF', 'NEK2', 'G2E3', 'GAS2L3', 'CBX5', 'CENPA'
+        ]
+    s_genes = cell_cycle_genes[:43]
+    g2m_genes = cell_cycle_genes[43:]
+
+    return s_genes, g2m_genes
+
 def score_genes_cell_cycle(
     adata: AnnData,
     s_genes: Sequence[str],
@@ -291,7 +311,20 @@ def score_genes_cell_cycle(
 
 
 
-def my_ssgsea(x: pd.Series, omega: float, gs):
+def my_ssgsea_adata(adata: AnnData, omega:float, gs):
+    """
+    operates on .raw!
+    """
+    scores = []
+    for i in tqdm.tqdm(range(adata.shape[0])):
+        df = pd.Series(
+                adata.raw.X[i,:].A.flatten(),
+                index=adata.raw.var.index
+            )
+        scores.append(my_ssgsea(df, omega=0, gs=gs))
+    return pd.Series(scores, index=adata.obs.index)  
+
+def my_ssgsea(x: pd.Series, omega: float, gs, plotting=False):
     """
     GSEA gene set scoring. See https://www.pathwaycommons.org/guide/primers/data_analysis/gsea/ for some details
     In brief:
@@ -351,6 +384,10 @@ def my_ssgsea(x: pd.Series, omega: float, gs):
         NO_vector[t] = 1 if not keys_sorted[t] in gene_set else 0
     Fi_not = np.cumsum(NO_vector) / (len(x) - len(gene_set))
 
+    if plotting:
+        import matplotlib.pyplot as plt
+        plt.plot(Fi-Fi_not)
+
     return max_deviation_from_zero(Fi - Fi_not)
 
 
@@ -370,3 +407,15 @@ def max_deviation_from_zero(x):
         return themax
     else:
         return themin
+
+
+def decoupler_cell_cycle_model():
+    """
+    turning the Cell cycle scoring into decoupler compatible input
+    """
+    s_genes, g2m_genes = get_cellcycle_genes()
+    model_cc = pd.concat([
+        pd.DataFrame({'source': 'Sphase', 'target':s_genes, 'weight': 1}),
+        pd.DataFrame({'source': 'G2Mphase', 'target': g2m_genes, 'weight': 1})
+    ])
+    return model_cc
